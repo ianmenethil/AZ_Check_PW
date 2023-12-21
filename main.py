24870fd06c58679ade45ef81773835048e15036d
Ian Menethil <62359909+ianmenethil@users.noreply.github.com>
2023-12-21 12:13:03 +1100

    master

--- main.py
+++ main.py
@@ -75,11 +75,7 @@ def get_access_token(credentials, console):
     return None
 
 
-def is_account_enabled(user_details):
-    """Check if the account is enabled or disabled."""
-    return user_details.get("accountEnabled", False)
-
-
+# trunk-ignore(pylint/W0613)
 def get_email_address(user_details, console):
     """Get the email address from the user details.
     Args:   user_details (dict): The dictionary containing user details.
@@ -98,9 +94,9 @@ def get_email_address(user_details, console):
         if email:
             if isinstance(email, list) and email:
                 email = email[0]  # Choose the first email if it's a list
-            console.log(f"Found email: {email} in field: {field}")
+            # console.log(f"Found email: {email} in field: {field}")
             return email
-    console.log("No email found in any of the fields")
+    # console.log("No email found in any of the fields")
     return None
 
 
@@ -118,7 +114,6 @@ def get_privileged_accounts(access_token, roles, role_data, console):
         privileged_accounts = []
         all_role_members = {}
         for role_id in roles:  # roles is now a list of role IDs
-            # console.log(f"Processing role: {role_id} in roles: {roles}")
             role_name = role_data.get(role_id, f"Unknown Role ({role_id})")
             role_endpoint = f"https://graph.microsoft.com/v1.0/directoryRoles/{role_id}/members"
             headers = {"Authorization": f"Bearer {access_token}"}
@@ -126,28 +121,26 @@ def get_privileged_accounts(access_token, roles, role_data, console):
             role_members = response.json().get("value", [])
             all_role_members[role_name] = role_members
 
-            # console.log(f"Processing role: {role_name}")
             if role_members:
                 console.log(f"Found {len(role_members)} members in role: {role_name}")
                 for member in role_members:
-                    # console.log(f"Member:{member}")
                     user_details_endpoint = f"https://graph.microsoft.com/v1.0/users/{member['id']}"
-                    # console.log(f"Getting user details for user ID: {member['id']} for member: {member}")
                     user_details_response = httpx.get(user_details_endpoint, headers=headers)
-                    # console.log(f"User details response: {user_details_response}")
                     user_details = user_details_response.json()
                     # console.log(json.dumps(user_details, indent=4))
-                    # if is_account_enabled(user_details):
+
                     email = get_email_address(user_details, console)
                     if email:
                         console.log(f"Checking email: {email} for privileged account.")
                         privileged_accounts.append(email)
                     else:
                         console.log(f"No email found for user ID: {member['id']}")
+
         # Save role members data
         save_json(all_role_members, ROLEMEMBERS_FILE)
         read_and_pretty_save(all_role_members, PRETTY_ROLEMEMBERS_FILE)
         return privileged_accounts
+
     except httpx.RequestError as err:
         console.log(f"[bold red]Failed to retrieve access token. Please check your credentials.: {err}")
         console.print_exception()
@@ -161,6 +154,16 @@ def get_privileged_accounts(access_token, roles, role_data, console):
     return None
 
 
+def is_account_enabled(user_details):
+    """Check if the account is enabled or disabled."""
+    return user_details.get("accountEnabled", False)
+
+
+def is_userType_member(user_details):
+    """Check if the account userType is member."""
+    return user_details.get("userType", "").lower() == "member"
+
+
 def get_user_details(access_token, user_email, console):
     """ Get user details from Microsoft Graph API.
     Args:
@@ -169,13 +172,30 @@ def get_user_details(access_token, user_email, console):
         console: Console object for logging.
     Returns: dict: User details in JSON format. """
     headers = {"Authorization": f"Bearer {access_token}"}
-    console.log(f"Getting user details for: {user_email}")
-    # user_endpoint = f"https://graph.microsoft.com/v1.0/users/{user_email}?$count=true&$select=displayName,mail,userPrincipalName,createdDateTime,usageLocation,userType,externalUserState,externalUserStateChangeDateTime,onPremisesSamAccountName,onPremisesSecurityIdentifier,onPremisesSyncEnabled,onPremisesUserPrincipalName,otherMails,passwordPolicies,onPremisesDistinguishedName,onPremisesDomainName,onPremisesLastSyncDateTime,refreshTokensValidFromDateTime,securityIdentifier,signInSessionsValidFromDateTime,id,passwordProfile,identities,signInActivity,accountEnabled{filters}"
+
     # user_endpoint = f"https://graph.microsoft.com/v1.0/users/{user_email}"
-    user_endpoint = f"https://graph.microsoft.com/beta/users/{user_email}"
-    # user_endpoint = f"https://graph.microsoft.com/beta/users/{user_email}?$filter=accountEnabled eq true and userType eq 'member'"
+    # user_endpoint = f"https://graph.microsoft.com/beta/users/{user_email}?$select=displayName,lastPasswordChangeDateTime"
+    user_endpoint = f"https://graph.microsoft.com/beta/users/{user_email}?$select=accountEnabled,userType,userPrincipalName,mail,mailNickname,displayName,givenName,surname,passwordProfile,passwordPolicies,lastPasswordChangeDateTime,refreshTokensValidFromDateTime,onPremisesUserPrincipalName,onPremisesSyncEnabled,onPremisesSamAccountName,onPremisesLastSyncDateTime,onPremisesDistinguishedName,creationType,createdDateTime,deletedDateTime,isResourceAccount,isManagementRestricted,otherMails,proxyAddresses,employeeId,employeeHireDate,employeeLeaveDateTime,employeeType,externalUserConvertedOn,externalUserState,externalUserStateChangeDateTime,cloudRealtimeCommunicationInfo,id,identities,provisionedPlans,assignedPlans,onPremisesSipInfo"
     response = httpx.get(user_endpoint, headers=headers)
-    return response.json()
+    user_details = response.json()
+    # console.log(f"User details: {json.dumps(user_details, indent=4)}")
+
+    if not is_account_enabled(user_details) or not is_userType_member(user_details):
+        console.log(
+            f"Skipping user details for: {user_email} | account {is_account_enabled(user_details)} | userType {is_userType_member(user_details)}")
+        return None
+
+    console.log(
+        f"User details for: {user_email} fetched successfully | Account enabled: {is_account_enabled(user_details)} | userType: {is_userType_member(user_details)}")
+
+    if 'assignedPlans' in user_details:  # Filter out 'Deleted' assignedPlans
+        user_details['assignedPlans'] = [plan for plan in user_details['assignedPlans']
+                                         if plan.get('capabilityStatus') != 'Deleted']
+
+    if 'provisionedPlans' in user_details:  # Filter out 'Deleted' provisionedPlans
+        user_details['provisionedPlans'] = [plan for plan in user_details['provisionedPlans']
+                                            if plan.get('capabilityStatus') != 'Deleted']
+    return user_details
 
 
 def save_json(data, filename):
@@ -184,23 +204,50 @@ def save_json(data, filename):
     file_content = json.dumps(data, indent=4)
     file_path.write_text(file_content, encoding="utf-8")
 
+# Use this to flatten all
+# def flatten_json(nested_json, exclude=None):
+#     """Flatten nested JSON object into a flat dictionary."""
+#     if exclude is None:
+#         exclude = [""]
+#     out = {}
 
-def flatten_json(nested_json, exclude=None):
+#     def flatten(x, name="", exclude=exclude):
+#         if type(x) is dict:
+#             for a in x:
+#                 if a not in exclude:
+#                     flatten(x[a], name + a + "_")
+#         elif type(x) is list:
+#             i = 0
+#             for a in x:
+#                 flatten(a, name + str(i) + "_")
+#                 i += 1
+#         else:
+#             out[name[:-1]] = x
+#     flatten(nested_json)
+#     return out
+
+
+def flatten_json(nested_json, complex_fields=None, exclude=None):
     """Flatten nested JSON object into a flat dictionary."""
+    if complex_fields is None:
+        complex_fields = ["assignedPlans", "provisionedPlans", "identities", "otherMails", "proxyAddresses"]
     if exclude is None:
         exclude = [""]
     out = {}
 
     def flatten(x, name="", exclude=exclude):
-        # trunk-ignore(ruff/E721)
         # trunk-ignore(pylint/C0123)
+        # trunk-ignore(ruff/E721)
         if type(x) is dict:
             for a in x:
                 if a not in exclude:
-                    flatten(x[a], name + a + "_")
-        # trunk-ignore(ruff/E721)
+                    if a in complex_fields:
+                        out[name + a] = json.dumps(x[a])
+                    else:
+                        flatten(x[a], name + a + "_")
         # trunk-ignore(pylint/C0123)
-        elif type(x) is list:
+        # trunk-ignore(ruff/E721)
+        elif type(x) is list and name[:-1] not in complex_fields:  # Flatten lists that are not complex fields
             i = 0
             for a in x:
                 flatten(a, name + str(i) + "_")
@@ -212,7 +259,7 @@ def flatten_json(nested_json, exclude=None):
 
 
 def read_and_pretty_save(data, output_file_path):
-    """Reads data, flattens it, and saves it to a CSV file."""
+    """Reads data, optionally reorders it, and saves it to a CSV file."""
     if isinstance(data, str):
         data = json.loads(data)
     flat_data = [flatten_json(item) for item in data]
@@ -223,16 +270,14 @@ def read_and_pretty_save(data, output_file_path):
 def main():
     """Entry point of the script."""
     console = Console()
-    with console.status("[bold green]Starting the script...[/]", spinner="dots"):
-        MEMBERS_DATADATE = (get_file_modification_date(ROLEMEMBERS_FILE)if ROLEMEMBERS_FILE_exist else None)
-        USER_DATADATE = (get_file_modification_date(USERDATA_FILE) if USERDATA_FILE_exist else None)
+    with console.status("[bold green]Running...[/]", spinner="dots"):
+        MEMBERS_DATADATE = (get_file_modification_date(ROLEMEMBERS_FILE)if ROLEMEMBERS_FILE_exist else None)  # Check for fresh data
+        USER_DATADATE = (get_file_modification_date(USERDATA_FILE) if USERDATA_FILE_exist else None)  # Check for fresh data
         if (ROLEMEMBERS_FILE_exist and USERDATA_FILE_exist and MEMBERS_DATADATE == TODAYSDATE):
             console.log("Role members files exist and are up to date. Skipping API calls.")
             return
         try:
             credentials = load_yaml(CONFIG_FILE)["azure_ad"]
-            # access_token = get_access_token(credentials, console)
-            # if access_token:
             if access_token := get_access_token(credentials, console):
                 role_data = load_json(ROLES_FILE)
                 roles = list(role_data.keys())
@@ -250,7 +295,8 @@ def main():
                         task = progress.add_task("[cyan]Fetching user details...", total=len(privileged_emails),)
                         for email in privileged_emails:
                             user_data = get_user_details(access_token, email, console)
-                            all_data.append(user_data)
+                            if user_data is not None:  # Check that user_data is not None before appending
+                                all_data.append(user_data)
                             progress.update(task, advance=1)
                     save_json(all_data, USERDATA_FILE)
                     read_and_pretty_save(all_data, PRETTY_USERDATA_FILE)
@@ -259,6 +305,7 @@ def main():
         except Exception as e:
             console.log(f"[bold red]An error occurred: {e}")
             console.print_exception()
+        console.log("[bold green]Goodbye.")
 
 
 if __name__ == "__main__":
